d9ef446c77f52db69d28d6a14e59e61f4806de77
KhanVl <vovahan2002@gmail.com>
2025-01-07 17:30:36 +0900

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -25,124 +25,162 @@ window.addEventListener('resize', initSwiper);
 window.addEventListener('load', initSwiper);
 
 
-
 const showButton1 = document.querySelector('.toggle-button:not(.hidden)');
 const hideButton1 = document.querySelector('.toggle-button.hidden');
 const additionalItems1 = document.querySelector('.content-text__more');
 
-// Обработчик для кнопки "Показать все"
 showButton1.addEventListener('click', function () {
     additionalItems1.style.display = 'inline-flex';
-    showButton1.style.display = 'none'; // Скрываем верхнюю кнопку
-    hideButton1.style.display = 'inline-flex'; // Показываем кнопку "Скрыть"
+    showButton1.style.display = 'none'; 
+    hideButton1.style.display = 'inline-flex'; 
 });
 
-// Обработчик для кнопки "Скрыть"
 hideButton1.addEventListener('click', function () {
     additionalItems1.style.display = 'none';
-    showButton1.style.display = 'inline-flex'; // Показываем верхнюю кнопку
-    hideButton1.style.display = 'none'; // Скрываем кнопку "Скрыть"
+    showButton1.style.display = 'inline-flex'; 
+    hideButton1.style.display = 'none';
 });
 
 
 
-// ============================================================= //
-
-// ============================================================================ //
-
-const showButton3 = document.querySelector('.section-type__button:not(.hidden)');
-const hideButton3 = document.querySelector('.section-type__button.hidden');
-const additionalItems3 = document.querySelector('.section-types__inner.second-block');
-showButton3.addEventListener('click', function () {
-    additionalItems3.style.display = 'flex';
-    showButton3.style.display = 'none'; // Скрываем верхнюю кнопку
-    hideButton3.style.display = 'flex'; // Показываем кнопку "Скрыть"
-});
-hideButton3.addEventListener('click', function () {
-    additionalItems3.style.display = 'none';
-    showButton3.style.display = 'flex'; // Показываем верхнюю кнопку
-    hideButton3.style.display = 'none'; // Скрываем кнопку "Скрыть"
-});
-
-
-
-document.querySelector('.app-header__burger').addEventListener('click', function(){
+document.querySelector('.app-header__burger').addEventListener('click', function(event) {
+    event.stopPropagation(); 
     this.classList.toggle('active');
     document.querySelector('.navigation-wrapper').classList.toggle('open');
     document.querySelector('.navigation__overlay').classList.toggle('open');
     document.querySelector('.navigation').classList.toggle('open');
-    
 });
 
-document.querySelector('.button.navigation__burger').addEventListener('click', function(){
-    this.classList.toggle('none');
-    document.querySelector('.navigation-wrapper').classList.toggle('open');
-    document.querySelector('.navigation__overlay').classList.toggle('open');
-    document.querySelector('.navigation').classList.toggle('open');
-    
+document.addEventListener('click', function(event) {
+    const navigationWrapper = document.querySelector('.navigation-wrapper');
+    const navigationBurger = document.querySelector('.navigation__burger');
+    const isClickInside = navigationWrapper.contains(event.target);
+    const isBurgerClick = navigationBurger.contains(event.target);
+
+    if (!isClickInside && !isBurgerClick) {
+        navigationWrapper.classList.remove('open');
+        document.querySelector('.navigation__overlay').classList.remove('open');
+        document.querySelector('.navigation').classList.remove('open');
+        navigationBurger.classList.remove('active');
+    }
 });
 
-document.querySelector('.button__item.message').addEventListener('click', function(){
-    this.classList.toggle('active');
+document.querySelectorAll('.button.navigation__burger, .button.popup__close, .button.call__close').forEach(function(element) {
+    element.addEventListener('click', function() {
+        this.classList.toggle('none');
+
+        if (this.classList.contains('navigation__burger')) {
+            document.querySelector('.navigation-wrapper').classList.toggle('open');
+            document.querySelector('.navigation__overlay').classList.toggle('open');
+            document.querySelector('.navigation').classList.toggle('open');
+        }
+
+        if (this.classList.contains('popup__close')) {
+            document.querySelector('.app-header__popup').classList.toggle('open');
+            document.querySelector('.app-header__wrapper').classList.toggle('open');
+            document.querySelector('.app-header__overlay').classList.toggle('open');
+        }
+
+        if (this.classList.contains('call__close')) {
+            document.querySelector('.app-header__call').classList.toggle('open');
+            document.querySelector('.app-header__call-wrapper').classList.toggle('open');
+            document.querySelector('.app-header__call-overlay').classList.toggle('open');
+        }
+    });
+});
+
+
+// Функция для переключения классов, управляющих видимостью окна сообщений
+function toggleMessageModal() {
     document.querySelector('.app-header__popup').classList.toggle('open');
     document.querySelector('.app-header__wrapper').classList.toggle('open');
     document.querySelector('.app-header__overlay').classList.toggle('open');
+}
+
+// Обработчик для кнопок вызова окна сообщений
+document.querySelectorAll('.button__item.message, .navigation-button.message').forEach(function (element) {
+    element.addEventListener('click', function () {
+        this.classList.toggle('active');
+        toggleMessageModal();
+    });
 });
 
-
-
-
-
-document.querySelector('.button.popup__close').addEventListener('click', function(){
-    this.classList.toggle('none');
-    document.querySelector('.app-header__popup').classList.toggle('open');
-    document.querySelector('.app-header__wrapper').classList.toggle('open');
-    document.querySelector('.app-header__overlay').classList.toggle('open');
-    
+// Обработчик для кнопки закрытия окна сообщений
+document.querySelector('.button.popup__close').addEventListener('click', function (event) {
+    event.preventDefault(); // Предотвращаем переход по ссылке
+    toggleMessageModal();
 });
 
-document.querySelector('.button__item.call').addEventListener('click', function(){
-    this.classList.toggle('active');
+// Обработчик для клика по затемнённому фону окна сообщений
+document.querySelector('.app-header__popup').addEventListener('click', function (event) {
+    if (event.target === this) {
+        toggleMessageModal();
+    }
+});
+
+// Функция для переключения классов, управляющих видимостью окна звонка
+function toggleCallModal() {
     document.querySelector('.app-header__call').classList.toggle('open');
     document.querySelector('.app-header__call-wrapper').classList.toggle('open');
     document.querySelector('.app-header__call-overlay').classList.toggle('open');
+}
+
+// Обработчик для кнопок вызова окна звонка
+document.querySelectorAll('.button__item.call, .navigation-button.call').forEach(function (element) {
+    element.addEventListener('click', function () {
+        this.classList.toggle('active');
+        toggleCallModal();
+    });
 });
 
-
-document.querySelector('.button.call__close').addEventListener('click', function(){
-    this.classList.toggle('none');
-    document.querySelector('.app-header__call').classList.toggle('open');
-    document.querySelector('.app-header__call-wrapper').classList.toggle('open');
-    document.querySelector('.app-header__call-overlay').classList.toggle('open');
-    
+// Обработчик для кнопки закрытия окна звонка
+document.querySelector('.button.call__close').addEventListener('click', function (event) {
+    event.preventDefault();
+    toggleCallModal();
 });
+
+// Обработчик для клика по затемнённому фону окна звонка
+document.querySelector('.app-header__call').addEventListener('click', function (event) {
+    if (event.target === this) {
+        toggleCallModal();
+    }
+});
+
 
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    const items = document.querySelectorAll('.brands-item');
-    const toggleButton = document.querySelector('.brands-button__more');
-    if (!toggleButton) {
-        console.warn('Элемент .brands-button__more не найден.');
-        return;
-    }
-    const buttonText = toggleButton.querySelector('.brands-button__hidden');
-    const buttonIcon = toggleButton.querySelector('.icon');
-    const visibleCount = 8;
-    let expanded = false;
+    function setupToggle(itemSelector, buttonSelector, buttonTextSelector, iconSelector, visibleCount) {
+        const items = document.querySelectorAll(itemSelector);
+        const toggleButton = document.querySelector(buttonSelector);
+        if (!toggleButton) {
+            console.warn(`Элемент ${buttonSelector} не найден.`);
+            return;
+        }
+        const buttonText = toggleButton.querySelector(buttonTextSelector);
+        const buttonIcon = toggleButton.querySelector(iconSelector);
+        let expanded = false;
 
-    items.forEach((item, index) => {
-        if (index < visibleCount) item.classList.add('visible');
-    });
-
-    if (items.length > visibleCount) toggleButton.style.display = 'flex';
-
-    toggleButton.addEventListener('click', function() {
-        expanded = !expanded;
         items.forEach((item, index) => {
-            if (index >= visibleCount) item.classList.toggle('visible', expanded);
+            if (index < visibleCount) item.classList.add('visible');
         });
-        buttonText.textContent = expanded ? 'Скрыть' : 'Показать все';
-        buttonIcon.classList.toggle('rotated', expanded);
-    });
+
+        if (items.length > visibleCount) toggleButton.style.display = 'flex';
+
+        toggleButton.addEventListener('click', function() {
+            expanded = !expanded;
+            items.forEach((item, index) => {
+                if (index >= visibleCount) item.classList.toggle('visible', expanded);
+            });
+            buttonText.textContent = expanded ? 'Скрыть' : 'Показать все';
+            buttonIcon.classList.toggle('rotated', expanded);
+        });
+    }
+
+    // Инициализация для первого набора элементов
+    setupToggle('.brands-item', '.brands-button__more', '.brands-button__hidden', '.icon', 8);
+
+    // Инициализация для второго набора элементов
+    setupToggle('.section-type', '.section-type__button', '.section-button__hidden', '.icon', 4);
 });
+
